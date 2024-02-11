adcfb2688910f4eb946aea877398249e42d30380
norba <norbaa79@gmail.com>
2024-02-11 11:19:42 +0100

    Initial commit

---
+++ src/test/java/hu/webler/weblerfirstliquibase/WeblerFirstLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerfirstliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerFirstLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
